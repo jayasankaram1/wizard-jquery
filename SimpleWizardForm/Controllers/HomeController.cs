0872771fd9e6ceae225dfa0bd85f25a4518f9950
jayasankaram1 <jayasankaram1@gmail.com>
2015-07-19 09:18:35 +0530

    firstommit

---
+++ SimpleWizardForm/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using SimpleWizardForm.Models;
+
+namespace SimpleWizardForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View("SimpleWizardForm");
+        }
+
+        //
+        // POST: /Home/
+        [HttpPost]
+        public ActionResult Index(SimpleWizardFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SimpleWizardFormSuccess");
+            }
+
+            // Just for testing in order to know when it is the serverside validation that has failed
+            ModelState.AddModelError("", "Server-side validation failed.");
+
+            // If we got this far something failed, redisplay form
+            return View("SimpleWizardForm", model);
+        }
+    }
+}
---
+++ SimpleWizardForm/Models/SimpleWizardFormModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleWizardForm.Models
+{
+    public class SimpleWizardFormModel : IValidatableObject
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "Postal code must consist of 4 digits.")]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "Home Phone")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Mobile Phone")]
+        public string Mobile { get; set; }
+
+        [Display(Name = "I'm at least 18 years old?")]
+        public bool HasTurned18 { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!HasTurned18)
+                yield return new ValidationResult("You must be 18 or older.", new[] { "HasTurned18" });
+        }
+    }
+}
